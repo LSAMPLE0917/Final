7876c5a4c2d6bf27ebd1f388f4c2131f0155d3bf
LSAMPLE0917 <Lmich457@hotmail.com>
2015-11-07 05:07:34 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'say/hello'
+
+  get 'say/goodbye'
+
   resources :users
   resources :posts
 
@@ -6,6 +10,9 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments
+
+    resources :hello
+    resources :goodbye
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
